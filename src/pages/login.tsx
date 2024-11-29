d93cd42a797463ad1e99e1d851e228b71e8b1d84
lcaputo <programadorcaputo@gmail.com>
2024-11-29 00:18:29 -0500

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -16,6 +16,9 @@ export default function Login() {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested"
       },
       body: JSON.stringify({
         email: email.value,
