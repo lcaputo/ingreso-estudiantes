95899d5d009b746df28dbcb9ce412ec1436f7643
Jnajera0430 <najerajoseantonio052@gmail.com>
2025-01-24 21:33:07 -0500

    solucion de cors

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -14,8 +14,10 @@ export default function Login() {
 
     const res = await fetch(VITE_API_URL + "/auth/login", {
       method: "POST",
+      mode: "cors",
       headers: {
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
       },
       body: JSON.stringify({
         email: email.value,
@@ -37,28 +39,28 @@ export default function Login() {
     <section id="login" className="bg-gray-50">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 h-screen">
         <span className="flex gap-2 mb-5">
-        <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="icon icon-tabler icon-tabler-fingerprint text-primary"
-        width="64"
-        height="64"
-        viewBox="0 0 24 24"
-        strokeWidth="2"
-        stroke="currentColor"
-        fill="none"
-        strokeLinecap="round"
-      >
-        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-        <path d="M18.9 7a8 8 0 0 1 1.1 5v1a6 6 0 0 0 .8 3" />
-        <path d="M8 11a4 4 0 0 1 8 0v1a10 10 0 0 0 2 6" />
-        <path d="M12 11v2a14 14 0 0 0 2.5 8" />
-        <path d="M8 15a18 18 0 0 0 1.8 6" />
-        <path d="M4.9 19a22 22 0 0 1 -.9 -7v-1a8 8 0 0 1 12 -6.95" />
-      </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-fingerprint text-primary"
+            width="64"
+            height="64"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            fill="none"
+            strokeLinecap="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M18.9 7a8 8 0 0 1 1.1 5v1a6 6 0 0 0 .8 3" />
+            <path d="M8 11a4 4 0 0 1 8 0v1a10 10 0 0 0 2 6" />
+            <path d="M12 11v2a14 14 0 0 0 2.5 8" />
+            <path d="M8 15a18 18 0 0 0 1.8 6" />
+            <path d="M4.9 19a22 22 0 0 1 -.9 -7v-1a8 8 0 0 1 12 -6.95" />
+          </svg>
 
-      <h1 className=" tracking-widest font-bold self-center text-4xl whitespace-nowrap text-secondary">
-        PIA
-      </h1>
+          <h1 className=" tracking-widest font-bold self-center text-4xl whitespace-nowrap text-secondary">
+            PIA
+          </h1>
         </span>
         <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
